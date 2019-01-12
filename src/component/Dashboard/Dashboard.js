d5c141b91e089f77f6f95904251fa9ea80ad3280
divya <divu333@gmail.com>
2019-01-11 23:25:28 -0700

    commit friday_initial_setup

---
+++ src/component/Dashboard/Dashboard.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+import Product from './component/Product/Product';
+
+
+
+class Dashboard extends Component {
+  render() {
+    return (
+    <div > 
+               <Product/>
+
+         Dashboard 
+         </div>
+    );
+  }
+}
+
+export default Dashboard;
---
+++ src/component/Form/Form.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react';
+
+
+class Form extends Component {
+  render() {
+    return <div > Home   </div>
+    
+  }
+}
+
+export default Form;
